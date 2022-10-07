cbe75ddb6e776c9df19d7d398db5deeed102dbaa
Paloma Alves <pa.oliveiraalves556@gmail.com>
2022-10-07 10:20:29 -0300

    Learning about variables in Ruby

---
+++ first_section/2_variaveis/2_variaveis.rb
@@ -0,0 +1,26 @@
+# VARIABLES
+
+#Variables are a way of assigning data to names in your programs. You can think 
+#of a variable as a box with a label on it: it stores something and has a name
+#so that you know what’s inside.
+
+#This is how to create a variable in Ruby:
+
+age = 18 #=> 18
+
+#You can also assign the result of an expression to a variable.
+
+age = 18 + 5 #=> 23
+
+#Variable names are reusable, so you can assign a new value to a variable at any point in your program. 
+#Naturally, doing so will override the original value.
+
+age = 18
+age #=> 18
+age = 33
+age #=> 33
+
+#HOW TO NAME VARIABLES
+#Ruby is a language that aims to be natural to read and easy to write.
+#Variable names should always be lowercase, and multiple words that make
+#up a variable name should be split by an underscore. This is known as snake_case.
