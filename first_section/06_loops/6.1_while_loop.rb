9b52b97a0b3b5f27bba8f6c5a83c097ed6fe1d91
Paloma Alves <pa.oliveiraalves556@gmail.com>
2022-10-17 16:04:13 -0300

    Learning about Loops

--- first_section/06_loops/6.1_while_loop.rb
+++ first_section/06_loops/6.1_while_loop.rb
@@ -1,1 +1,19 @@
-#
+#WHILE LOOP
+
+# A while loop is given a parameter that evaluates as either true or false. Once that 
+#expression becomes false, the while loop is not executed again, and the program continues
+#after the while loop. Code within the while loop can contain any kind of logic that you
+#would like to perform.
+
+i = 0
+while i < 10 do
+ puts "i is #{i}"
+ i += 1
+end
+
+#You can also use while loops to repeatedly ask a question of the user until they give 
+#the desired response:
+
+while gets.chomp != "yes" do
+  puts "Will you go to prom with me?"
+end
