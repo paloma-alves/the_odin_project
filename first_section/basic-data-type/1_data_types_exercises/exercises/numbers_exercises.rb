f0d66ffb2fb063858a07aebb10950b36992f0f9d
Paloma Alves <pa.oliveiraalves556@gmail.com>
2023-07-07 11:54:50 -0300

    redoing exercises

--- first_section/basic-data-type/1_data_types_exercises/exercises/numbers_exercises.rb
+++ first_section/basic-data-type/1_data_types_exercises/exercises/numbers_exercises.rb
@@ -1,71 +1,63 @@
-def add(a, b)
-  return a + b  
+def add(a,b)
+  return a + b
 end
 
-def subtract(a, b)
- return a - b 
-  # return the result of subtracting b from a
+def subtracts(a,b)
+  return a - b
 end
 
-def multiply(a, b)
- return a * b
-  # return the result of multiplying a times b
+def multiply(a,b)
+  return a * b
 end
 
-def divide(a, b)
+def divide(a,b)
   return a / b
-  # return the result of dividing a by b
 end
 
-def remainder(a, b)
+def remainder(a,b)
   return a % b
-  # return the remainder of dividing a by b using the modulo operator
 end
 
-def float_division(a, b)
-  return (a / b).to_f
-  # return the result of dividing a by b as a float, rather than an integer
+def float_division(a,b)
+  return a.to_f / b.to_f
 end
 
-def string_to_number(string)
-  return string.to_i
-  # return the result of converting a string into an integer
+def string_to_number(string) #duvida
+  return string.to_i 
 end
 
-def even?(number)
- return number.even?  
-  # return true if the number is even (hint: use integer's even? method)
+def even?(a) #a means number
+  return a.even?
 end
 
-def odd?(number)
-  return number.odd?
-  # return true if the number is odd (hint: use integer's odd? method)
+def odd?(a) #a means number
+  return a.odd?
 end
 
-def transform_negative (number)
-  return -1 * (number)
+def transform_negative(a) #a means number
+  return a * -1
 end
 
-def transform_positive (number)
-  return -1 * (number)
+def transform_positive(a) #a means number
+  return a * -1 #because - + - = +
 end
 
-def triple_multiplication (a, b, c)
+def triple_multiplication (a,b,c)
   return a * b * c
 end
 
-def square (number)
- return (number) ** 2 
+def square(a,b)
+  return a ** b
 end
 
-def square_perimeter (number)
-  return 4 * (number)
+def square_perimeter(polygn)
+  def polygn * 4
 end
 
-def rectangle_perimeter (a, b)
-  return  (a * 2) + (b * 2)
+def rectangle_perimeter (a,b) #duvida
+  return (a * 2) * (b * 2)
 end
 
-def percentage (a, b)
-  return (b * 100) / a 
-end
+def percentage(a,b)
+  return (a / b) * 100
+end 
