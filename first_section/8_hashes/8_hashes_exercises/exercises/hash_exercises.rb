ce922c01a457eb3a67de26c54cb5deedd603750a
Paloma Alves <pa.oliveiraalves556@gmail.com>
2023-07-14 19:10:56 -0300

    Hash exercises finished and solved

--- first_section/8_hashes/8_hashes_exercises/exercises/hash_exercises.rb
+++ first_section/8_hashes/8_hashes_exercises/exercises/hash_exercises.rb
@@ -3,6 +3,36 @@ def create_favorite_hash(color, number)
   {color: color, number: number}
 end
 
+def favorite_color(hash)
+    hash[:color]
+end
+
+def favorite_number(hash)
+    hash[:number] || 42
+end
+
+def update_favorite_movie(hash,value)
+  hash[:movie] = value
+  hash
+end
+
+def remove_favorite_number(hash)
+    hash.delete(:number)
+    hash
+end
+
+def favorite_categories(hash)
+    hash.keys
+end
+
+def favorite_items(hash)
+    hash.values
+end
+
+def merge_favorites(hash1, hash2)
+    hash1.merge(hash2)
+end
+
 
 
 
